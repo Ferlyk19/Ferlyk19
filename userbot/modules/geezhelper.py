e7e66e8e312c4d81578a5140ab14749bfb74f8ad
Ferlyk19 <87972799+Ferlyk19@users.noreply.github.com>
2021-08-19 06:54:09 +0700

    Update geezhelper.py

--- userbot/modules/geezhelper.py
+++ userbot/modules/geezhelper.py
@@ -12,9 +12,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/VckyouuBitch)"
-        "\n[Repo](https://github.com/Vckyou/Geez-UserBot)"
-        "\n[Instagram](instagram.com/Vckyouuu)")
+        "\n[Telegram](t.me/FK190202)"
+        "\n[Repo](https://github.com/Ferlyk19/ferlyk19)"
+        "\n[Instagram](instagram.com/er.19__)")
 
 
 @register(outgoing=True, pattern="^.vars$")
@@ -27,7 +27,7 @@ async def var(m):
 CMD_HELP.update({
     "helper":
     "`.lhelp`\
-\nUsage: Bantuan Untuk Geez-Project.\
+\nUsage: Bantuan Untuk Ferly-Project.\
 \n`.vars`\
 \nUsage: Melihat Daftar Vars."
 })
